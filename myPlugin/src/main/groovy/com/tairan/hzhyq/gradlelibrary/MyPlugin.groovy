6b328d3c2155bd8b396e38a4deaff87b695ba65a
vinci <coderhuang2016@gmail.com>
2017-04-03 22:54:30 +0800

    plugin hook aroud the task execution

--- myPlugin/src/main/groovy/com/tairan/hzhyq/gradlelibrary/MyPlugin.groovy
+++ myPlugin/src/main/groovy/com/tairan/hzhyq/gradlelibrary/MyPlugin.groovy
@@ -2,14 +2,34 @@ package com.tairan.hzhyq.gradlelibrary
 
 import org.gradle.api.Project
 import org.gradle.api.Plugin
+import org.gradle.api.Task
+import org.gradle.api.execution.TaskExecutionGraph
+import org.gradle.api.execution.TaskExecutionListener
+import org.gradle.api.tasks.TaskState
+import org.gradle.execution.TaskGraphExecuter
 
-public class MyPlugin implements Plugin<Project> {
+public class MyPlugin implements Plugin<Project>,TaskExecutionListener {
     @Override
     void apply(Project project) {
         project.extensions.create('pluginExtension', PluginExtension)
 
         project.extensions.create('custorm',CustormExtension)
         project.task('myTask',type: MyTask)
+
+    }
+
+    @Override
+    void beforeExecute(Task task) {
+        if(task.name == 'assembleRelease'){
+            println "${task.name} start to execute"
+        }
+    }
+
+    @Override
+    void afterExecute(Task task, TaskState taskState) {
+        if(task.name == 'assembleRelease'){
+            println "${task.name} end"
+        }
     }
 }
 class CustormExtension{
